731bb8ebecb087157cf5cad70141072105f56aee
Aimann <aimannin212@gmail.com>
2025-01-31 15:27:42 -0800

    initial commit

--- test_data/run-pipeline.sh
+++ test_data/run-pipeline.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-## run the snakemake pipeline
+config_file="$1"   # path to config file
 
-# Exit on error
-set -e
+# get the input_dir from the config file trim whitespace after the word. Remove "" from the input_dir
+input_dir=$(grep -m 1 "input_dir" "$config_file" | awk '{print $2}' | tr -d '"')
+printf "input_dir: %s\n" "$input_dir"
 
-# Activate conda environment
-# This path might differ based on your Miniconda installation in the container
-source /opt/conda/etc/profile.d/conda.sh
-conda activate ribomarker
+# get the output_prefix from the config file trim whitespace after the word. Remove "" from the output_prefix
+output_prefix=$(grep -m 1 "output_prefix" "$config_file" | awk '{print $2}' | tr -d '"')
+printf "output_prefix: %s\n" "$output_prefix"
 
-# Print what we are about to run (helpful in debugging)
-echo "Running Snakemake with arguments: $@"
+threads=$(grep -m 1 "threads" "$config_file" | awk '{print $2}' | tr -d '"')
+## check the system cores
+if [ -z "$threads" ]; then
+  cores=$(nproc)
+else
+  cores="$threads"
+fi
+printf "cores: %s\n" "$cores"
 
-snakemake --unlock
+## get cwd
+cwd=$(pwd)
 
-# ##just run the pipeline, no docker
-snakemake --configfile config.yaml --cores 20 --rerun-incomplete 2>&1 | tee /pipeline/data/snakemake.log
+config_file="${cwd}/${config_file}"
+output_dir="${cwd}/${output_prefix}"
+
+docker run --rm \
+  -v "${input_dir}":/pipeline/data \
+  -v "${config_file}":/pipeline/config.yaml \
+  -v "${output_dir}":/pipeline/"${output_prefix}" \
+  -w /pipeline \
+  snax \
+  --cores "${cores}" \
+  "$@"
